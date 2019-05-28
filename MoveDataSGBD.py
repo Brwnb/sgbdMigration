fa4c856485197c316a5fc493c24706548345d3c5
Bruno Borges <bruno2.0oliveira@gmail.com>
2019-05-27 23:19:25 -0300

    Versão 0.2

--- MoveDataSGBD.py
+++ MoveDataSGBD.py
@@ -44,19 +44,33 @@ def ajusta(data):
 		l = l + i
 	return l
 
-for i in conn_mssql.execute("select username, birthday from client"):  
-        cpf = i[0]
-        data = i[1]
-        data_nasc = ajusta(data)
-        conn_mysql=connection_mysql.cursor()
-        sql= ("insert into radcheck (username, value) values (%s,%s)")
-        conn_mysql.execute(sql, (cpf, data_nasc))
-        connection_mysql.commit()
-      
+# A idéia é verificar se existe o cpf existe ou se ele já está 
+# inserido no radius
+def verifica(cpf):
+	boleano = True
+	conn_mysql=connection_mysql.cursor()
+	sql = ("select username from radcheck")
+	lista = conn_mysql.execute(sql)
+	for i in lista:
+		if lista[0] == cpf or lista[0] == None or lista[0] == '':
+			boleano = False
+			a = conn_mysql.close()
+			break
+		elif lista[0] != cpf:
+			boleano = True
+	a = conn_mysql.close()
+	return boleano
+
+# Nesse for eu faço um select no banco mssql, chamo a função verifica para verficar se o cpf existe.
+# Se existir ele retorna Falso e então não inseri no banco freeradius.
+for i in conn_mssql.execute("select username, birthday from client"):
+	if verifica(i[0]) == True:
+		data_nasc = ajusta(i[1])
+		conn_mysql=connection_mysql.cursor()
+		sql= ("insert into radcheck (username, value) values (%s,%s)")
+		conn_mysql.execute(sql, (i[0], data_nasc))
+		connection_mysql.commit()
+
 conn_mssql.close()
-a = conn_mysql.close()
 connection_mssql.close()
 connection_mysql.close()
-
-
-
